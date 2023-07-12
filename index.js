31ec7db1502e4dd3f1e5195d419dbbdef5b26e2a
JackLee6 <jasonglobguo@gmail.com>
2023-07-11 20:32:27 -0400

    Events for YouTube
    Added more events for YouTube synchronization

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const io = require('socket.io')(server)
 const Lobby = require('./yt_lobby.js')
 // Make a Lobby instance called 'lobby'
 const lobby = new Lobby('test')
+// To handle registering new player states
+let prevState = -5
+let prevRuntime = 0
 
 io.on('connection', (socket) => {
     console.log('A client connected')
+    socket.join('lobby1')
     // Testing
     socket.on('buttonClick', () => {
         socket.emit('serverResponse', 'Bing Chilling')
@@ -17,6 +21,10 @@ io.on('connection', (socket) => {
     socket.on('reconnection', () => {
         socket.emit('playVideo', lobby.playlist[0])
         socket.emit('updatePlaylist', lobby.playlist)
+        if(prevState !== -5) {
+            // console.log("Sending old runtime information: previous state = " + prevState + " prev time = " + prevRuntime)
+            socket.emit('receiveInfo', prevState, prevRuntime)
+        }
     })
     // Client will send 'addVideo' when the user inputs a youtube video link and requests to add it to the list
     socket.on('addVideo', (video_id) => {
@@ -28,21 +36,37 @@ io.on('connection', (socket) => {
         // If the list is empty, we need to make sure the client knows to render the new video added
         if(length == 1) {
             // if length == 1, then that means before the video added, the list was empty
-            socket.emit('playVideo', lobby.playlist[0])
+            // socket.emit('playVideo', lobby.playlist[0])
+            io.to('lobby1').emit('playVideo', lobby.playlist[0])
         }
-        socket.emit('updatePlaylist', lobby.playlist);
+        // socket.emit('updatePlaylist', lobby.playlist);
+        io.to('lobby1').emit('updatePlaylist', lobby.playlist)
     })
     // Client will send 'goNext' when either the video ends, or user requests to skip
     socket.on('goNext', () => {
         console.log("Received request to play next video")
         lobby.removeVideo()
-        socket.emit('playVideo', lobby.playlist[0])
-        socket.emit('updatePlaylist', lobby.playlist);
+        // socket.emit('playVideo', lobby.playlist[0])
+        // socket.emit('updatePlaylist', lobby.playlist)
+        io.to('lobby1').emit('playVideo', lobby.playlist[0])
+        io.to('lobby1').emit('updatePlaylist', lobby.playlist)
     })
     // Client will send 'toggleLoop' when they click the 'Loop' button
     socket.on('toggleLoop', () => {
         lobby.toggleLoop()
         console.log("Loop Toggled: ", lobby.loop)
+    })
+    socket.on('playbackChange', (playbackState, playbackTime) => {
+        console.log("Playback State Received: ", playbackState)
+        if(prevState !== playbackState) {
+            prevState = playbackState
+            console.log("Change detected: sending new info...")
+            // socket.emit('receiveInfo', playbackState, playbackTime)
+            io.to('lobby1').emit('receiveInfo', playbackState, playbackTime)
+        }
+    })
+    socket.on('updateTime', (playerTime) => {
+        prevRuntime = playerTime
     })
     // Sent automatically when the client disconnects from the server
     socket.on('disconnect', () => {
